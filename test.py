3bc7f031f0bb591a4981046f0f5b6cf46408e35c
yszoke <szoke@post.bgu.ac.il>
2020-12-25 18:36:18 +0200

    10 cross (LR is the best)

--- test.py
+++ test.py
@@ -25,69 +25,131 @@ from sklearn.model_selection import train_test_split
 # nltk.download('averaged_perceptron_tagger')
 # nltk.download('wordnet')
 from nltk.corpus import sentiwordnet as swn
-
-
+import matplotlib.pyplot as plt
 
 
 def test():
     df = pd.read_csv('Train.csv',encoding='latin-1')
     df.groupby('Sentiment').SentimentText.count().plot.bar(ylim=0)
     plt.show()
-
+################################################################
     stemmer = PorterStemmer()
     words = stopwords.words("english")
 
     df['processedtext'] = df['SentimentText'].apply(
         lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", x).split() if i not in words]).lower())
-    # print(df.shape)
-    # df.head(10)
 
     target = df['Sentiment']
+################################################################
+    X_train, X_test, y_train, y_test = train_test_split(df['processedtext'], target, test_size=0.20, random_state=100)
 
-    X_train, X_test, y_train, y_test = train_test_split(df['processedtext'], target, test_size=0.30, random_state=100)
-
-    print("shape: ",df.shape);
-    print("train shape: ",X_train.shape);
-    print("test shape: ",X_test.shape)
+    # print("shape: ",df.shape);
+    # print("train shape: ",X_train.shape);
+    # print("test shape: ",X_test.shape)
 
     vectorizer_tfidf = TfidfVectorizer(stop_words='english', max_df=0.7)
     train_tfIdf = vectorizer_tfidf.fit_transform(X_train.values.astype('U'))
-
     test_tfIdf = vectorizer_tfidf.transform(X_test.values.astype('U'))
 
     print("features names: ",vectorizer_tfidf.get_feature_names()[:10])
     print("train tf idf shape: ",train_tfIdf.shape)
     print("test tf idf shape: ",test_tfIdf.shape,"\n")
 
-    print("NB:")
-    nb_classifier = MultinomialNB()
+    ################################################################
+    import pandas
+    from sklearn import model_selection
+    from sklearn.metrics import classification_report
+    from sklearn.metrics import confusion_matrix
+    from sklearn.metrics import accuracy_score
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.svm import SVC
+    from sklearn import metrics
 
-    nb_classifier.fit(train_tfIdf, y_train)
+    scoring = 'accuracy'
+    seed = 7
+    # 10 cross validation to check wich model to choose
 
-    pred2 = nb_classifier.predict(test_tfIdf)
-    # print(pred2[:10])
+    models = []
+    models.append(('LR', LogisticRegression(solver='liblinear')))
+    # models.append(('LDA', LinearDiscriminantAnalysis()))
+    models.append(('KNN', KNeighborsClassifier()))
+    # models.append(('CART', DecisionTreeClassifier()))
+    models.append(('NB', MultinomialNB()))
+    # models.append(('SVM', SVC()))
 
-    # Calculate the accuracy score: score
-    accuracy_tfidf = metrics.accuracy_score(y_test, pred2)
-    print(accuracy_tfidf)
+    results = []
+    names = []
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=None)
+        cv_results = model_selection.cross_val_score(model, train_tfIdf, y_train, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
 
-    Conf_metrics_tfidf = metrics.confusion_matrix(y_test, pred2, labels=[1, 0])
-    print(Conf_metrics_tfidf,"\n")
+    ######################### NB ###########################
 
-    print("Random Forest Classifier:")
-    classifier = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=100)
+    # print("NB:")
+    # nb_classifier = MultinomialNB()
+    #
+    # nb_classifier.fit(train_tfIdf, y_train)
+    #
+    # pred2 = nb_classifier.predict(test_tfIdf)
+    # # print(pred2[:10])
+    #
+    # # Calculate the accuracy score: score
+    # accuracy_tfidf = metrics.accuracy_score(y_test, pred2)
+    # print(accuracy_tfidf)
 
-    classifier.fit(train_tfIdf, y_train)
+    ######################### LR ###########################
+    # print("LR:")
+    # lr_classifier = LogisticRegression(solver='liblinear')
+    #
+    # lr_classifier.fit(train_tfIdf, y_train)
+    #
+    # pred3 = lr_classifier.predict(test_tfIdf)
+    # # print(pred2[:10])
+    #
+    # # Calculate the accuracy score: score
+    # accuracy_tfidf = metrics.accuracy_score(y_test, pred3)
+    # print(accuracy_tfidf)
 
-    predRF = classifier.predict(test_tfIdf)
-    # print(predRF[:10])
+    #################### EXECUTE ###########################
+    # print("***test***")
+    # df2 = pd.read_csv('Test.csv',encoding='latin-1')
+    # temp= df2['SentimentText'].apply(
+    #     lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", x).split() if i not in words]).lower())
+    # test_x=temp
+    # test_tfIdf2 = vectorizer_tfidf.transform(test_x.values.astype('U'))
+    # # pred_test=nb_classifier.predict(test_tfIdf2)
+    # pred_test = lr_classifier.predict(test_tfIdf2)
+    # df2['SentimentText'] = pred_test
+    # df2.columns = ['ID','Sentiment']
+    # df2.to_csv("results3.csv",index=False)
+    #
+    #
+    # Conf_metrics_tfidf = metrics.confusion_matrix(y_test, pred2, labels=[1, 0])
+    # print(Conf_metrics_tfidf,"\n")
 
-    # Calculate the accuracy score
-    accuracy_RF = metrics.accuracy_score(y_test, predRF)
-    print(accuracy_RF)
-
-    Conf_metrics_RF = metrics.confusion_matrix(y_test, predRF, labels=[1, 0])
-    print(Conf_metrics_RF)
+    ######################### RFC ###########################
+    # print("Random Forest Classifier:")
+    # classifier = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=100)
+    #
+    # classifier.fit(train_tfIdf, y_train)
+    #
+    # predRF = classifier.predict(test_tfIdf)
+    # # print(predRF[:10])
+    #
+    # # Calculate the accuracy score
+    # accuracy_RF = metrics.accuracy_score(y_test, predRF)
+    # print(accuracy_RF)
+    #
+    # Conf_metrics_RF = metrics.confusion_matrix(y_test, predRF, labels=[1, 0])
+    # print(Conf_metrics_RF)
 
 
 if __name__ == "__main__":
